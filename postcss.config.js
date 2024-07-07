1157a4caae72303fc584a5f1b6d98f6b5db4052e
Evgenii-Dolgopolov <do.evgenii@gmail.com>
2024-07-07 23:13:41 +0700

    clean up folder structure

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,7 @@
-import tailwindcss from 'tailwindcss';
-import typography from '@tailwindcss/typography';
-import autoprefixer from 'autoprefixer';
+import tailwindcss from "tailwindcss"
+import typography from "@tailwindcss/typography"
+import autoprefixer from "autoprefixer"
 
 export default {
-  plugins: [
-    tailwindcss,
-    typography,
-    autoprefixer,
-  ],
-};
+  plugins: [tailwindcss, typography, autoprefixer],
+}
