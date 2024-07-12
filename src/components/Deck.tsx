e43ddc0da08f7320ca03690902f085e77d07d7dd
Evgenii-Dolgopolov <do.evgenii@gmail.com>
2024-07-12 22:59:33 +0700

    improve styling of the decks

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -16,10 +16,10 @@ const Deck: React.FC<DeckProps> = ({ deck }) => {
   const { Name: name, Description: description } = deck
 
   return (
-    <div className="flex flex-1 max-h-40 h-40 w-full bg-yellow-200 p-4 rounded-md shadow-md">
-      <div className="flex flex-col gap-6">
-        <div className="font-bold text-lg">Name: {name}</div>
-        <div className="text-sm">Description: {description}</div>
+    <div className="flex flex-1 max-h-40 h-40 w-full bg-yellow-200 rounded-md shadow-md">
+      <div className="flex flex-col items-center justify-center flex-1 gap-4">
+        <div className="text-center font-bold text-lg px-6">{name}</div>
+        <div className="text-center text-sm px-6 max-w-md">{description}</div>
       </div>
     </div>
   )
