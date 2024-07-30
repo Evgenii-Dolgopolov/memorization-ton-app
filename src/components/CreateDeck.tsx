fe11b38c4a5bf0af9a9a9a78f2f7f0038f2beb3a
Evgenii-Dolgopolov <do.evgenii@gmail.com>
2024-07-30 13:45:37 +0700

    MEM-13 add edit deck functionality

--- src/components/CreateDeck.tsx
+++ src/components/CreateDeck.tsx
@@ -98,12 +98,12 @@ const CreateDeck = ({ onCreate }: { onCreate: (deck: Deck) => void }) => {
             value={description}
             onChange={e => setDescription(e.target.value)}
             maxLength={70}
-            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm resize-none"
           />
         </div>
         <button
           type="submit"
-          className="mt-4 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          className="mt-4 flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           Create Deck
         </button>
       </div>
--- src/pages/DeckDetails.tsx
+++ src/pages/DeckDetails.tsx
@@ -17,19 +17,22 @@ const DeckDetails: React.FC = () => {
     return data.map((item: any) => ({
       deckId: item.ID,
       question: item.Question,
-      answer: item.Answer
+      answer: item.Answer,
     }))
   }
 
   useEffect(() => {
     const fetchCards = async () => {
       try {
-        const response = await fetch(`http://localhost:8080/decks/${deckId}/cards`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
+        const response = await fetch(
+          `http://localhost:8080/decks/${deckId}/cards`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
 
         const data = await response.json()
         console.log("Fetched data:", data)
@@ -62,13 +65,42 @@ const DeckDetails: React.FC = () => {
   }
 
   return (
-    <div className="">
-      {cards.map(card => (
-        <div key={card.deckId} className="bg-orange-300">
-          <h2>Question: {card.question}</h2>
-          <h1>Answer: {card.answer}</h1>
+    <div className="p-8 min-h-screen flex flex-col gap-6 bg-purple-300">
+      <h1 className="flex flex-col gap-4 text-center text-4xl font-bold">
+        Your Cards
+      </h1>
+
+      <div className="flex gap-4">
+        <button className="flex-1 inline-flex justify-center px-4 py-2 border border-transparent text-md font-bold rounded-md shadow-lg text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          Edit Deck
+        </button>
+
+        <button className="flex-1 inline-flex justify-center px-4 py-2 border border-transparent text-md font-bold rounded-md shadow-lg text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          Add Card
+        </button>
+      </div>
+
+      <div className="bg-green-300">
+        <div
+          className="grid grid-cols-3 gap-4 bg-blue-200 p-2"
+          style={{ gridTemplateColumns: "1fr 1fr 0.25fr" }}>
+          <div className="font-bold">Question</div>
+          <div className="font-bold">Answer</div>
+          <div className="font-bold flex items-center justify-center">Edit</div>
         </div>
-      ))}
+        {cards.map(card => (
+          <div
+            key={card.deckId}
+            className="grid grid-cols-3 gap-4 bg-pink-300 p-2 my-2"
+            style={{ gridTemplateColumns: "1fr 1fr 0.25fr" }}>
+            <div className="text-sm">{card.question}</div>
+            <div className="text-sm">{card.answer}</div>
+            <button className="flex items-center justify-center">
+              <i className="fa-solid fa-pen-to-square"></i>
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
